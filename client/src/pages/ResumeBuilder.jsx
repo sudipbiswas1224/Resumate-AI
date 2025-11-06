ba03d3ca5751b19a29d54dd260d35ffb742d1635
Sudip Biswas <12345biswassudip@gmail.com>
2025-11-06 13:26:19 +0530

    Personal_Info Done

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,66 +1,182 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { dummyResumeData } from '../assets/assets';
-import { ArrowLeft, ArrowLeftIcon, Briefcase, FileText, FolderIcon, GraduationCap, Sparkle, Sparkles, User } from 'lucide-react';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import {
+  ArrowLeft,
+  ArrowLeftIcon,
+  Briefcase,
+  ChevronLeft,
+  ChevronRight,
+  FileText,
+  FolderIcon,
+  GraduationCap,
+  Sparkle,
+  Sparkles,
+  User,
+} from "lucide-react";
+import PersonalInfoForm from "../components/PersonalInfoForm";
+import ResumePreview from "../components/ResumePreview";
+import TemplateSelector from "../components/TemplateSelector";
+import ColorPicker from "../components/ColorPicker";
 
 const ResumeBuilder = () => {
-  const {resumeId} = useParams();
+  const { resumeId } = useParams();
   const [resumeData, setResumeData] = useState({
-    _id:"",
-    title:"",
-    professional_info:{},
-    professional_info:"",
-    experience:[],
-    project:[],
-    skills:[],
-    template:"classic",
-    accent_color:"#3b82F6",
-    public:false
-  })
+    _id: "",
+    title: "",
+    personal_info: {},
+    professional_info: "",
+    experience: [],
+    project: [],
+    skills: [],
+    template: "classic",
+    accent_color: "#3b82F6",
+    public: false,
+  });
 
-  const loadExistingResume = async () =>{
-    const resume = dummyResumeData.find(resume =>  resume._id == resumeId);
-    if(resume) {
+  const loadExistingResume = async () => {
+    const resume = dummyResumeData.find((resume) => resume._id == resumeId);
+    if (resume) {
       setResumeData(resume);
       document.title = resume.title;
     }
-  }
+  };
+
+  //state to choose the section index
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [removeBackground, setRemoveBackground] = useState(false);
 
   const sections = [
-    { id:"personal", name: "Personal Info", icon: User},
-    { id:"summary", name: "summary", icon: FileText},
-    { id:"experience", name: "Experience", icon: Briefcase},
-    { id:"education", name: "Education", icon: GraduationCap},
-    { id:"projects", name: "Projects", icon: FolderIcon},
-    { id:"skills", name: "Skills", icon: Sparkles},
-  ]
+    { id: "personal", name: "Personal Info", icon: User },
+    { id: "summary", name: "summary", icon: FileText },
+    { id: "experience", name: "Experience", icon: Briefcase },
+    { id: "education", name: "Education", icon: GraduationCap },
+    { id: "projects", name: "Projects", icon: FolderIcon },
+    { id: "skills", name: "Skills", icon: Sparkles },
+  ];
+  const activeSection = sections[activeSectionIndex];
 
-  useEffect(()=>{
-    loadExistingResume()
-  },[])
+  useEffect(() => {
+    loadExistingResume();
+  }, []);
   return (
     <div>
-        <div className='max-w-7xl  mx-auto px-4 py-6'>
-          {/* Back to Dash board */}
-          <Link to='/app' className='inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all '>
-            <ArrowLeftIcon className='size-4' /> Back to Dashboard
-          </Link>
-        </div>
+      <div className="max-w-7xl  mx-auto px-4 py-6">
+        {/* Back to Dash board */}
+        <Link
+          to="/app"
+          className="inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all "
+        >
+          <ArrowLeftIcon className="size-4" /> Back to Dashboard
+        </Link>
+      </div>
 
-        <div className='max-w-7xl mx-auto px-4 pb-8'>
-          <div className='grid lg:grid-cols-2 gap-8' >
-            {/* Left Panel -Form */}
+      <div className="max-w-7xl mx-auto px-4 pb-8">
+        <div className="grid lg:grid-cols-12 gap-8">
+          {/* Left Panel -Form */}
+          <div className="lg:col-span-5 relative rounded-lg overflow-hidden">
+            <div className="bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1">
+              {/* progress bar using active section index */}
+              <hr className=" absolute top-0 left-0 right-0 border-2 border-gray-200" />
+              <hr
+                className="absolute top-0 left-0 h-1 bg-linear-to-r from-green-500 to-green-600 border-none transition-all duration-2000"
+                style={{
+                  width: `${
+                    (activeSectionIndex * 100) / (sections.length - 1)
+                  }%`,
+                }}
+              />
+
+              {/* Section Navigation button */}
+              <div className="flex items-center justify-between mb-6 border-b border-gray-300 py-1">
+                {/* templates */}
+                <div className="flex items-center gap-2 ">
+                  <TemplateSelector
+                    selectedTemplate={resumeData.template}
+                    onChange={(template) =>
+                      setResumeData((prev) => ({ ...prev, template }))
+                    }
+                  />
+
+                  {/* accent color picker */}
+                  <ColorPicker
+                    selectedColor={resumeData.accent_color}
+                    onChange={(color) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        accent_color: color,
+                      }))
+                    }
+                  />
+                </div>
+
+                {/* next and previous button */}
+                <div className="flex items-center">
+                  {/* previous btn */}
+                  {activeSectionIndex !== 0 && (
+                    <button
+                      onClick={() =>
+                        setActiveSectionIndex((prev) => Math.max(prev - 1, 0))
+                      }
+                      className="flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all duration-300"
+                      disabled={activeSectionIndex === 0}
+                    >
+                      <ChevronLeft className="size-4" /> Previous
+                    </button>
+                  )}
+
+                  {/* next btn */}
+                  <button
+                    onClick={() =>
+                      setActiveSectionIndex((prev) =>
+                        Math.min(prev + 1, sections.length - 1)
+                      )
+                    }
+                    className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all duration-300 ${
+                      activeSectionIndex === sections.length - 1 && "opacity-50"
+                    }`}
+                    disabled={activeSectionIndex === sections.length - 1}
+                  >
+                    Next <ChevronRight className="size-4" />
+                  </button>
+                </div>
+              </div>
+
+              {/* form content */}
+              <div className="space-y-6">
+                {activeSection.id === "personal" && (
+                  <PersonalInfoForm
+                    data={resumeData.personal_info}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        personal_info: data,
+                      }))
+                    }
+                    removeBackground={removeBackground}
+                    setRemoveBackground={setRemoveBackground}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+
+          {/* Right panel - Preview */}
+          <div className="lg:col-span-7 max-lg:mt-6">
+            {/* buttons */}
             <div></div>
 
-            {/* Right panel - Preview */}
-            <div></div>
-
+            {/* resume preview */}
+            <ResumePreview
+              data={resumeData}
+              template={resumeData.template}
+              accentColor={resumeData.accent_color}
+            />
           </div>
         </div>
-
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResumeBuilder
+export default ResumeBuilder;
